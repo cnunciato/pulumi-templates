e438d7384d84cc96e6ff978b71f14beeae5f56e2
Christian Nunciato <c@nunciato.org>
2022-08-21 12:49:56 -0700

    Set configs by cloud, activatae venv for Python

--- static-website/scripts/deploy.sh
+++ static-website/scripts/deploy.sh
@@ -5,13 +5,29 @@ source ./scripts/common.sh
 
 for cloud in $CLOUDS; do
     for lang in $LANGUAGES; do
-        pulumi -C "$cloud/$lang" stack init dev || pulumi -C "$cloud/$lang" stack select dev
-        pulumi -C "$cloud/$lang" config set aws:region us-west-2
-        pulumi -C "$cloud/$lang" config set azure-native:location WestUS
-        pulumi -C "$cloud/$lang" config set gcp:project pulumi-development
-        pulumi -C "$cloud/$lang" config set path ./site
-        pulumi -C "$cloud/$lang" config set indexDocument index.html
-        pulumi -C "$cloud/$lang" config set errorDocument error.html
-        pulumi -C "$cloud/$lang" up --yes
+        pushd "$cloud/$lang"
+            pulumi stack init dev || pulumi stack select dev
+
+            if [ "$cloud" == "aws" ]; then
+                pulumi config set aws:region us-west-2
+            fi
+
+            if [ "$cloud" == "azure" ]; then
+                pulumi config set azure-native:location WestUS
+            fi
+
+            if [ "$cloud" == "gcp" ]; then
+                pulumi config set gcp:project pulumi-development
+            fi
+
+            if [ "$lang" == "python" ]; then
+                source bin/activate
+            fi
+
+            pulumi config set path ./site
+            pulumi config set indexDocument index.html
+            pulumi config set errorDocument error.html
+            pulumi up --yes
+        popd
     done
 done
